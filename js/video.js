a0a261cbf55b0f7f67e62634fce86919ceca0175
nawaphonf1 <nwp2000f@gmail.com>
2023-01-29 12:13:02 +0700

    แก้

--- js/video.js
+++ js/video.js
@@ -1,394 +1,151 @@
+//กดเลือกวิดีโอ เปอเซนต์ และห้ามกรอ
+var videos = [
+  {
+    id: 1,
+    name: "1.1 แนะนำเว็บแอปพลิเคชัน อพม.Smart",
+    src: "video/1.1 แนะนำเว็บแอปพลิเคชัน อพม.Smart.mp4",
+    watched: false,
+  },
+  {
+    id: 2,
+    name: "2.1 การเข้าสู่ระบบ",
+    src: "video/2.1 การเข้าสู่ระบบ.mp4",
+    watched: false,
+  },
+  {
+    id: 3,
+    name: "3.1 การยื่นคำร้องและการกดรับพิกัด GPS",
+    src: "video/3.1 การยื่นคำร้องและการกดรับพิกัด GPS.mp4",
+    watched: false,
+  },
+  {
+    id: 4,
+    name: "3.2 การกรอกประวัติผู้ประสบปัญหาทางสังคม",
+    src: "video/3.2 การกรอกประวัติผู้ประสบปัญหาทางสังคม.mp4",
+    watched: false,
+  },
+  {
+    id: 5,
+    name: "3.3 การกรอกที่อยู่ตามทะเบียนบ้าน",
+    src: "video/3.3 การกรอกที่อยู่ตามทะเบียนบ้าน.mp4",
+    watched: false,
+  },
+  {
+    id: 6,
+    name: "3.4 การกรอกที่อยู่ปัจจุบัน",
+    src: "video/3.4 การกรอกที่อยู่ปัจจุบัน.mp4",
+    watched: false,
+  },
+  {
+    id: 7,
+    name: "3.5การเลือกประเภทผู้ขอรับบริการ",
+    src: "video/3.5การเลือกประเภทผู้ขอรับบริการ.mp4",
+    watched: false,
+  },
+  {
+    id: 8,
+    name: "3.6 การเลือกสภาพปัญหาความเดือดร้อนที่พบ",
+    src: "video/3.6 การเลือกสภาพปัญหาความเดือดร้อนที่พบ.mp4",
+    watched: false,
+  },
+  {
+    id: 9,
+    name: "3.7 การเลือกเรื่องที่ขอรับความช่วยเหลือที่ต้องการ",
+    src: "video/3.7 การเลือกเรื่องที่ขอรับความช่วยเหลือที่ต้องการ.mp4",
+    watched: false,
+  },
+  {
+    id: 10,
+    name: "3.8 สิทธิสวัสดิการที่เคยได้รับ",
+    src: "video/3.8 สิทธิสวัสดิการที่เคยได้รับ.mp4",
+    watched: false,
+  },
+  {
+    id: 11,
+    name: "3.9 การรับรองข้อมูลส่วนบุคคล",
+    src: "video/3.9 การรับรองข้อมูลส่วนบุคคล.mp4",
+    watched: false,
+  },
+  {
+    id: 12,
+    name: "3.10 การเยี่ยมบ้านและการแนบเอกสารเพิ่มเติม",
+    src: "video/3.10 การเยี่ยมบ้านและการแนบเอกสารเพิ่มเติม.mp4",
+    watched: false,
+  },
+  {
+    id: 13,
+    name: "3.11 การยืนยันการบันทึกข้อมูล",
+    src: "video/3.11 การยืนยันการบันทึกข้อมูล.mp4",
+    watched: false,
+  },
+  {
+    id: 14,
+    name: "4.1 การตรวจสอบและบันทึกข้อมูลสำหรับ อพม.ระดับตำบล",
+    src: "video/4.1 การตรวจสอบและบันทึกข้อมูลสำหรับ อพม.ระดับตำบล.mp4",
+    watched: false,
+  },
+  {
+    id: 15,
+    name: "4.2 การตรวจสอบและบันทึกข้อมูลสำหรับ อพม.ระดับอำเภอ",
+    src: "video/4.2 การตรวจสอบและบันทึกข้อมูลสำหรับ อพม.ระดับอำเภอ.mp4",
+    watched: false,
+  },
+  {
+    id: 16,
+    name: "5.1 ความหมายของสถานะ",
+    src: "video/5.1 ความหมายของสถานะ.mp4",
+    watched: false,
+  },
+  {
+    id: 17,
+    name: "6.1 การสร้างทางลัด อพม.Smart ไว้บนหน้าจอหลัก",
+    src: "video/6.1 การสร้างทางลัด อพม.Smart ไว้บนหน้าจอหลัก.mp4",
+    watched: false,
+  },
+];
 
-//่กดเลือกวิดิโอ
-var video1 = false,
-    video2 = false,
-    video3 = false,
-    video4 = false,
-    video5 = false,
-    video6 = false,
-    video7 = false,
-    video8 = false,
-    video9 = false,
-    video10 = false,
-    video11 = false,
-    video12 = false,
-    video13 = false,
-    video14 = false,
-    video15 = false,
-    video16 = false,
-    video17 = false;
-
-if(video1==false){
-  document.getElementById("videoLink4").style.pointerEvents = "none";
-}else{
-  document.getElementById("videoLink4").style.pointerEvents = "auto";
-}
-
+var player = videojs("videoP");
 function light(Cvideo) {
-  let player = videojs("videoP");
-  var video;
+  for (let i = 0; i < videos.length; i++) {
+    if (videos[i].id === Cvideo) {
+      document.getElementById("nameV").innerHTML = videos[i].name;
+      player.src({ type: "video/mp4", src: videos[i].src });
+      player.play();
 
-  //เปลี่ยนวิดีโอ1
-  if (Cvideo == 1) {
-    document.getElementById("nameV").innerHTML =
-      "1.1 แนะนำเว็บแอปพลิเคชัน อพม.Smart";
-    video = "video/1.1 แนะนำเว็บแอปพลิเคชัน อพม.Smart.mp4";
+      if (!videos[i].watched) {
+        player.controlBar.progressControl.hide();
 
-    if ((video1 == false)) {
-      player.controlBar.progressControl.hide();
-      player.on("timeupdate", function () {
-        var percentage = (player.currentTime() / player.duration()) * 100;
-        document.getElementById("percentage").innerHTML =
-          Math.round(percentage) + "%";
-        if (percentage == 100) {
-          video1 = true;
-          return;
-        }
-      });
-    } else if ((video1 == true)) {
-      player.controlBar.progressControl.show();
+        player.on("timeupdate", function () {
+          var percentage = (player.currentTime() / player.duration()) * 100;
+          console.log(percentage);
+          document.getElementById("percentage").innerHTML =
+            Math.round(percentage) + "%";
+          if (percentage === 100) {
+            videos[i].watched = true;
+            console.log(videos[0].watched);
+            return;
+          }
+        });
+      } else {
+        player.controlBar.progressControl.show();
+      }
+      break;
     }
-
-  //เปลี่ยนวิิดีโอ 2
-  } else if (Cvideo == 2) {
-    document.getElementById("nameV").innerHTML = "2.1 การเข้าสู่ระบบ";
-    video = "video/2.1 การเข้าสู่ระบบ.mp4";
-
-    if ((video2 == false)) {
-      player.controlBar.progressControl.hide();
-      player.on("timeupdate", function () {
-        var percentage = (player.currentTime() / player.duration()) * 100;
-        document.getElementById("percentage").innerHTML =
-          Math.round(percentage) + "%";
-        if (percentage == 100) {
-          video2 = true;
-          return;
-        }
-      });
-    } else if ((video2 == true)) {
-      player.controlBar.progressControl.show();
-    }
-  //เปลี่ยนวิดีโอ3
-  } else if (Cvideo == 3) {
-    document.getElementById("nameV").innerHTML =
-      "3.1 การยื่นคำร้องและการกดรับพิกัด GPS";
-    video = "video/3.1 การยื่นคำร้องและการกดรับพิกัด GPS.mp4";
-
-    if ((video3 == false)) {
-      player.controlBar.progressControl.hide();
-      player.on("timeupdate", function () {
-        var percentage = (player.currentTime() / player.duration()) * 100;
-        document.getElementById("percentage").innerHTML =
-          Math.round(percentage) + "%";
-        if (percentage == 100) {
-          video3 = true;
-          return;
-        }
-      });
-    } else if ((video3 == true)) {
-      player.controlBar.progressControl.show();
-    }
-  //เปลี่ยนวิดีโอ4
-  } else if (Cvideo == 4) {
-    document.getElementById("nameV").innerHTML =
-      "3.2 การกรอกประวัติผู้ประสบปัญหาทางสังคม";
-    video = "video/3.2 การกรอกประวัติผู้ประสบปัญหาทางสังคม.mp4";
-
-    if ((video4 == false)) {
-      player.controlBar.progressControl.hide();
-      player.on("timeupdate", function () {
-        var percentage = (player.currentTime() / player.duration()) * 100;
-        document.getElementById("percentage").innerHTML =
-          Math.round(percentage) + "%";
-        if (percentage == 100) {
-          video4 = true;
-          return;
-        }
-      });
-    } else if ((video4 == true)) {
-      player.controlBar.progressControl.show();
-    }
-
-  //เปลี่ยนวิดีโอ5
-  } else if (Cvideo == 5) {
-    document.getElementById("nameV").innerHTML =
-      "3.3 การกรอกที่อยู่ตามทะเบียนบ้าน";
-    video = "video/3.3 การกรอกที่อยู่ตามทะเบียนบ้าน.mp4";
-
-    if ((video5 == false)) {
-      player.controlBar.progressControl.hide();
-      player.on("timeupdate", function () {
-        var percentage = (player.currentTime() / player.duration()) * 100;
-        document.getElementById("percentage").innerHTML =
-          Math.round(percentage) + "%";
-        if (percentage == 100) {
-          video5 = true;
-          return;
-        }
-      });
-    } else if ((video5 == true)) {
-      player.controlBar.progressControl.show();
-    }
-
-  //เปลี่ยนวิดีโอ6
-  } else if (Cvideo == 6) {
-    document.getElementById("nameV").innerHTML = "3.4 การกรอกที่อยู่ปัจจุบัน";
-    video = "video/3.4 การกรอกที่อยู่ปัจจุบัน.mp4";
-
-    if ((video6 == false)) {
-      player.controlBar.progressControl.hide();
-      player.on("timeupdate", function () {
-        var percentage = (player.currentTime() / player.duration()) * 100;
-        document.getElementById("percentage").innerHTML =
-          Math.round(percentage) + "%";
-        if (percentage == 100) {
-          video6 = true;
-          return;
-        }
-      });
-    } else if ((video6 == true)) {
-      player.controlBar.progressControl.show();
-    }
-
-  //เปลี่ยนวิดีโอ7
-  } else if (Cvideo == 7) {
-    document.getElementById("nameV").innerHTML =
-      "3.5 การเลือกประภทผู้ขอรับบริการ";
-    video = "video/3.5การเลือกประเภทผู้ขอรับบริการ.mp4";
-
-    if ((video7 == false)) {
-      player.controlBar.progressControl.hide();
-      player.on("timeupdate", function () {
-        var percentage = (player.currentTime() / player.duration()) * 100;
-        document.getElementById("percentage").innerHTML =
-          Math.round(percentage) + "%";
-        if (percentage == 100) {
-          video7 = true;
-          return;
-        }
-      });
-    } else if ((video7 == true)) {
-      player.controlBar.progressControl.show();
-    }
-
-  //เปลี่ยนวิดีโอ 8
-  } else if (Cvideo == 8) {
-    document.getElementById("nameV").innerHTML =
-      "3.6 การเลือกสภาพปัญหาความเดือดร้อนที่พบ";
-    video = "video/3.6 การเลือกสภาพปัญหาความเดือดร้อนที่พบ.mp4";
-
-    if ((video8 == false)) {
-      player.controlBar.progressControl.hide();
-      player.on("timeupdate", function () {
-        var percentage = (player.currentTime() / player.duration()) * 100;
-        document.getElementById("percentage").innerHTML =
-          Math.round(percentage) + "%";
-        if (percentage == 100) {
-          video8 = true;
-          return;
-        }
-      });
-    } else if ((video8 == true)) {
-      player.controlBar.progressControl.show();
-    }
-
-  //เปลี่ยนวิดีโอ 9
-  } else if (Cvideo == 9) {
-    document.getElementById("nameV").innerHTML =
-      "3.7 การเลือกเรื่องที่ขอรับความช่วยเหลือที่ต้องการ";
-    video = "video/3.7 การเลือกเรื่องที่ขอรับความช่วยเหลือที่ต้องการ.mp4";
-
-    if ((video9 == false)) {
-      player.controlBar.progressControl.hide();
-      player.on("timeupdate", function () {
-        var percentage = (player.currentTime() / player.duration()) * 100;
-        document.getElementById("percentage").innerHTML =
-          Math.round(percentage) + "%";
-        if (percentage == 100) {
-          video9 = true;
-          return;
-        }
-      });
-    } else if ((video9 == true)) {
-      player.controlBar.progressControl.show();
-    }
-
-  //เปลี่ยนวิดีโอ10
-  } else if (Cvideo == 10) {
-    document.getElementById("nameV").innerHTML =
-      "3.8 สิทธิสวัสดิการที่เคยได้รับ";
-    video = "video/3.8 สิทธิสวัสดิการที่เคยได้รับ.mp4";
-
-    if ((video10 == false)) {
-      player.controlBar.progressControl.hide();
-      player.on("timeupdate", function () {
-        var percentage = (player.currentTime() / player.duration()) * 100;
-        document.getElementById("percentage").innerHTML =
-          Math.round(percentage) + "%";
-        if (percentage == 100) {
-          video10 = true;
-          return;
-        }
-      });
-    } else if ((video10 == true)) {
-      player.controlBar.progressControl.show();
-    }
-
-  //เปลี่ยนวิดีโอ11
-  } else if (Cvideo == 11) {
-    document.getElementById("nameV").innerHTML = "3.9 การรับรองข้อมูลส่วนบุคคล";
-    video = "video/3.9 การรับรองข้อมูลส่วนบุคคล.mp4";
-
-    if ((video11 == false)) {
-      player.controlBar.progressControl.hide();
-      player.on("timeupdate", function () {
-        var percentage = (player.currentTime() / player.duration()) * 100;
-        document.getElementById("percentage").innerHTML =
-          Math.round(percentage) + "%";
-        if (percentage == 100) {
-          video11 = true;
-          return;
-        }
-      });
-    } else if ((video11 == true)) {
-      player.controlBar.progressControl.show();
-    }
-
-  //เปลี่ยนวิดีโอ12
-  } else if (Cvideo == 12) {
-    document.getElementById("nameV").innerHTML =
-      "3.10 การเยี่ยมบ้านและการแนบเอกสารเพิ่มเติม";
-    video = "video/3.10 การเยี่ยมบ้านและการแนบเอกสารเพิ่มเติม.mp4";
-
-    if ((video12 == false)) {
-      player.controlBar.progressControl.hide();
-      player.on("timeupdate", function () {
-        var percentage = (player.currentTime() / player.duration()) * 100;
-        document.getElementById("percentage").innerHTML =
-          Math.round(percentage) + "%";
-        if (percentage == 100) {
-          video12 = true;
-          return;
-        }
-      });
-    } else if ((video12 == true)) {
-      player.controlBar.progressControl.show();
-    }
-
-  //เปลี่ยนวิดีโอ 13
-  } else if (Cvideo == 13) {
-    document.getElementById("nameV").innerHTML =
-      "3.11 การยืนยันการบันทึกข้อมูล";
-    video = "video/3.11 การยืนยันการบันทึกข้อมูล.mp4";
-
-    if ((video13 == false)) {
-      player.controlBar.progressControl.hide();
-      player.on("timeupdate", function () {
-        var percentage = (player.currentTime() / player.duration()) * 100;
-        document.getElementById("percentage").innerHTML =
-          Math.round(percentage) + "%";
-        if (percentage == 100) {
-          video13 = true;
-          return;
-        }
-      });
-    } else if ((video13 == true)) {
-      player.controlBar.progressControl.show();
-    }
-
-  //เปลี่ยนวิดีโอ14
-  } else if (Cvideo == 14) {
-    document.getElementById("nameV").innerHTML =
-      "4.1 การตรวจสอบและบันทึกข้อมูลสำหรับ อพม.ระดับตำบล";
-    video = "video/4.1 การตรวจสอบและบันทึกข้อมูลสำหรับ อพม.ระดับตำบล.mp4";
-    
-    if ((video14 == false)) {
-      player.controlBar.progressControl.hide();
-      player.on("timeupdate", function () {
-        var percentage = (player.currentTime() / player.duration()) * 100;
-        document.getElementById("percentage").innerHTML =
-          Math.round(percentage) + "%";
-        if (percentage == 100) {
-          video14 = true;
-          return;
-        }
-      });
-    } else if ((video14 == true)) {
-      player.controlBar.progressControl.show();
-    }
-
-  //เปลี่ยนวิดีโอ 15
-  } else if (Cvideo == 15) {
-    document.getElementById("nameV").innerHTML =
-      "4.2 การตรวจสอบและบันทึกข้อมูลสำหรับ อพม.ระดับอำเภอ";
-    video = "video/4.2 การตรวจสอบและบันทึกข้อมูลสำหรับ อพม.ระดับอำเภอ.mp4";
-
-    if ((video15 == false)) {
-      player.controlBar.progressControl.hide();
-      player.on("timeupdate", function () {
-        var percentage = (player.currentTime() / player.duration()) * 100;
-        document.getElementById("percentage").innerHTML =
-          Math.round(percentage) + "%";
-        if (percentage == 100) {
-          video15 = true;
-          return;
-        }
-      });
-    } else if ((video15 == true)) {
-      player.controlBar.progressControl.show();
-    }
-
-  //เปลี่ยนวิดีโอ16
-  } else if (Cvideo == 16) {
-    document.getElementById("nameV").innerHTML = "5.1 ความหมายของสถานะ";
-    video = "video/5.1 ความหมายของสถานะ.mp4";
-
-    if ((video16 == false)) {
-      player.controlBar.progressControl.hide();
-      player.on("timeupdate", function () {
-        var percentage = (player.currentTime() / player.duration()) * 100;
-        document.getElementById("percentage").innerHTML =
-          Math.round(percentage) + "%";
-        if (percentage == 100) {
-          video16 = true;
-          return;
-        }
-      });
-    } else if ((video16 == true)) {
-      player.controlBar.progressControl.show();
-    }
-
-
-  //เปลี่ยนวิดีโอ17
-  } else if (Cvideo == 17) {
-    document.getElementById("nameV").innerHTML =
-      "6.1 การสร้างทางลัด อพม.Smart ไว้บนหน้าจอหลัก";
-    video = "video/6.1 การสร้างทางลัด อพม.Smart ไว้บนหน้าจอหลัก.mp4";
-
-    if ((video17 == false)) {
-      player.controlBar.progressControl.hide();
-      player.on("timeupdate", function () {
-        var percentage = (player.currentTime() / player.duration()) * 100;
-        document.getElementById("percentage").innerHTML =
-          Math.round(percentage) + "%";
-        if (percentage == 100) {
-          video17 = true;
-          return;
-        }
-      });
-    } else if ((video17 == true)) {
-      player.controlBar.progressControl.show();
-    }
-
   }
-  player.src({ type: "video/mp4", src: video });
-  player.play();
-}
-function t2(){
-  document.getElementById("nameV").innerHTML =video+10;
 }
 
-/* //เปอเซนต์และห้ามกรอ
+for (let v = 2; v <= 11; v++) {
+  
+  if (videos[v].watched==false) {
+    document.getElementById("videoLink" + [v + 2]).style.pointerEvents = "none";
+    
+    
+  } else if (videos[v].watched == true) {
+    document.getElementById("videoLink" + [v + 2]).style.pointerEvents = "auto";
+  }
+}
+
+/* 
 var player = videojs("videoP");
 var video1 = false;
 player.on("timeupdate", function () {
@@ -434,5 +191,3 @@ function search() {
     }
   }
 }
-
-
